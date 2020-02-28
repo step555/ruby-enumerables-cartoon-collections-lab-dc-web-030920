dae036cf9d6ae198502903dacfa0eb49402447c1
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-28 22:59:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)
-  numbered = []
+  # numbered = []
   array.each_with_index { |item, index| puts "#{index+1}#{item}" }
-  numbered
+  # numbered
 end
 
 def summon_captain_planet(array)
